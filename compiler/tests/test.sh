eef2b5a32bcb443719fd3658ec41addcdb5a1707
toiki <tuiboku1128@gmail.com>
2022-04-06 00:01:56 +0900

    add testcase

--- compiler/tests/test.sh
+++ compiler/tests/test.sh
@@ -16,14 +16,34 @@ assert(){
     fi
 }
 
-assert 27 "5 + 23 - 1"
-assert 25 "10 + 5*3"
-assert 2 "10/(1+4)"
+assert 0 0
+assert 42 42
+assert 21 '5+20-4'
+assert 41 ' 12 + 34 - 5 '
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
 assert 10 '-10+20'
-assert 20 '10--10'
-assert 0 '-10-(-10)'
+assert 10 '- -10'
+assert 10 '- - +10'
+
+assert 0 '0==1'
+assert 1 '42==42'
+assert 1 '0!=1'
+assert 0 '42!=42'
+
+assert 1 '0<1'
+assert 0 '1<1'
+assert 0 '2<1'
+assert 1 '0<=1'
+assert 1 '1<=1'
+assert 0 '2<=1'
+
+assert 1 '1>0'
+assert 0 '1>1'
+assert 0 '1>2'
+assert 1 '1>=0'
+assert 1 '1>=1'
+assert 1 '(3+4)*10 >= 2*(1+3)'
 
 echo OK
