b2be03ae50950b0a7f923ab30b3f3d0dbd62b72e
toiki <tuiboku1128@gmail.com>
2022-04-07 00:44:08 +0900

    impl parse and asm gen of var assign

--- compiler/src/parse.rs
+++ compiler/src/parse.rs
@@ -5,15 +5,17 @@ use crate::token::Token;
 
 #[derive(Debug)]
 pub enum NodeType {
-    ND_ADD,
-    ND_SUB,
-    ND_MUL,
-    ND_DIV,
-    ND_EQ,
-    ND_NE,
-    ND_LT,
-    ND_LE,
-    ND_NUM,
+    ND_ADD,    // +
+    ND_SUB,    // -
+    ND_MUL,    // *
+    ND_DIV,    // /
+    ND_EQ,     // ==
+    ND_NE,     // !=
+    ND_LT,     // <
+    ND_LE,     // <=
+    ND_ASSIGN, // =
+    ND_LVAR,   // ローカル変数
+    ND_NUM,    // 整数
 }
 
 #[derive(Debug)]
@@ -22,6 +24,7 @@ pub struct Node {
     pub lhs: Option<Box<Node>>,
     pub rhs: Option<Box<Node>>,
     pub val: i32,
+    pub offset: u8,
 }
 
 pub fn new_node(node_type: NodeType, lhs: Box<Node>, rhs: Box<Node>) -> Node {
@@ -30,6 +33,7 @@ pub fn new_node(node_type: NodeType, lhs: Box<Node>, rhs: Box<Node>) -> Node {
         lhs: Some(lhs),
         rhs: Some(rhs),
         val: 0,
+        offset: 0,
     }
 }
 
@@ -39,14 +43,41 @@ pub fn new_num_node(val: i32) -> Node {
         lhs: None,
         rhs: None,
         val: val,
+        offset: 0,
     }
 }
 
-pub fn expr(iter: &mut Peekable<IntoIter<Token>>) -> Node {
-    return equality(iter);
+pub fn program(iter: &mut Peekable<IntoIter<Token>>, code: &mut Vec<Node>) {
+    while !token::at_eof(iter) {
+        println!("{:?}", iter.peek());
+        code.push(stmt(iter));
+        iter.next();
+    }
+}
+
+fn stmt(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("stmt: {:?}", iter.peek());
+    let node = expr(iter);
+    token::expect(';', iter);
+    node
+}
+
+fn expr(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("expr: {:?}", iter.peek());
+    return assign(iter);
+}
+
+fn assign(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("assign: {:?}", iter.peek());
+    let mut node = equality(iter);
+    if token::consume("=", iter) {
+        node = new_node(NodeType::ND_ASSIGN, Box::new(node), Box::new(assign(iter)));
+    }
+    node
 }
 
 pub fn equality(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("equality: {:?}", iter.peek());
     let mut node = relational(iter);
     loop {
         if token::consume("==", iter) {
@@ -60,6 +91,7 @@ pub fn equality(iter: &mut Peekable<IntoIter<Token>>) -> Node {
 }
 
 pub fn relational(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("relational: {:?}", iter.peek());
     let mut node = add(iter);
 
     loop {
@@ -78,6 +110,7 @@ pub fn relational(iter: &mut Peekable<IntoIter<Token>>) -> Node {
 }
 
 pub fn add(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("add: {:?}", iter.peek());
     let mut node = mul(iter);
 
     loop {
@@ -92,6 +125,7 @@ pub fn add(iter: &mut Peekable<IntoIter<Token>>) -> Node {
 }
 
 pub fn mul(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("mul: {:?}", iter.peek());
     let mut node = unary(iter);
 
     loop {
@@ -106,6 +140,7 @@ pub fn mul(iter: &mut Peekable<IntoIter<Token>>) -> Node {
 }
 
 pub fn unary(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("unary: {:?}", iter.peek());
     if token::consume("+", iter) {
         return primary(iter);
     } else if token::consume("-", iter) {
@@ -119,6 +154,19 @@ pub fn unary(iter: &mut Peekable<IntoIter<Token>>) -> Node {
 }
 
 pub fn primary(iter: &mut Peekable<IntoIter<Token>>) -> Node {
+    println!("primary: {:?}", iter.peek());
+    if token::consume_ident("=", iter) {
+        let offset = (iter.peek().unwrap().str.as_bytes()[0] - 'a' as u8 + 1) * 8;
+        let node = Node {
+            node_type: NodeType::ND_LVAR,
+            lhs: None,
+            rhs: None,
+            val: 0,
+            offset: offset,
+        };
+        return node;
+    }
+
     if token::consume("(", iter) {
         let node = expr(iter);
         token::expect(')', iter);
